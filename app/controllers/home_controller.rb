f59a297fb6d82734216443ac734c3db9da0c31ea
Drew Harris <drew@the1drewharris.com>
2014-04-09 16:36:37 -0500

    more small changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -102,7 +102,7 @@ class HomeController < ApplicationController
       
       hotel = HotelSearch.new(params)
       
-      @hotels = Hotel.search
+      @hotels = Hotel.search(params[:search])
       # @hotels = Hotel.all
       
       session[:hotel_ids] = []
