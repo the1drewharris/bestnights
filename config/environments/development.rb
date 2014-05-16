e7bf723e89f86a379ea1dfb96285bea55d2c88cc
Surajit Dey <surajitd@clariontechnologies.co.in>
2014-05-16 14:33:22 +0530

    host address updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@
   # Expands the lines which load the assets
   config.assets.debug = true
   
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }  
+  config.action_mailer.default_url_options = { :host => '10.0.0.126:2222' }  
   
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
