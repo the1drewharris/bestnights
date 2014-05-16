30e292b855d302d2e459dca4f2a4665baf1aa444
Surajit Dey <surajitd@clariontechnologies.co.in>
2014-05-16 12:23:59 +0530

    Email configuaration updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Bestnights::Application.configure do
+  Bestnights::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
@@ -32,18 +32,19 @@ Bestnights::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
   
-  config.action_mailer.default_url_options = { :host => 'bestnights.com' }  
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }  
   
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-   address: "mail.web-host.net",
-   port: 2525,
-   authentication: "login",
-   enable_starttls_auto: false,
-   user_name: "customerservice@bestnights.com",
-   password: "Jamesbrown01"
+   address: "smtp.gmail.com",
+   port: 587,
+   domain: "gmail.com",
+   authentication: "plain",
+   enable_starttls_auto: true,
+   user_name:"indusnet.testacc@gmail.com",
+   password:"test@123"
 
     }
 
