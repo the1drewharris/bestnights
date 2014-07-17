14b22fcf7e745f772f26f74933995ddb37ca3c49
surajit-int <surajit.dey@indusnet.co.in>
2014-07-17 14:04:53 +0530

    Traveler checkout issue resolved and some client's issue resolved

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -345,7 +345,7 @@ class HomeController < ApplicationController
       results.each do |line|
       chars += line.length
     end
-    @fax_email = FaxMailer.hotel_booking_mail(traveler, amount, cardnumber, ccv, expiration, cardtype, hotel, checkin, checkout, room_ids).deliver
+  
     @fax_result = SOAP::WSDLDriverFactory.new("https://ws-sl.fax.tc/Outbound.asmx?WSDL").create_rpc_driver.SendCharFax("Username" => "bestnights","Password" => "@BestN1ghts","FileType" => "TXT","FaxNumber"=> "15874090031","Data" => "#{results[0]+"\n"+results[1]+"\n"+results[2]+"\n"+results[3]+"\n"+results[4]+"\n"+results[5]+"\n"+results[6]+"\n"+results[7]+"\n"+results[8]}")
     logger.info"@@@@@@@@@@@@@@@@@@@@@@@#{@fax_result.inspect}@@@@@@@@@@@@@@@@@@@@@@@@"
     unless @fax_result["SendCharFaxResult"].include? "-"
