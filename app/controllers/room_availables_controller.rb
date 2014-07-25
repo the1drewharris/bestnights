36393b6763dfb65cd73de194b20a26c840b37f8b
Ujjal <ujjal@ujjal.(none)>
2014-07-25 15:24:46 +0530

    Fixed overview page issue

--- app/controllers/room_availables_controller.rb
+++ app/controllers/room_availables_controller.rb
@@ -13,10 +13,33 @@ layout "admin_basic"
 		@rooms = []
 		if !params[:room_id].blank? 
 			params[:room_id].each do |room|
-				@room = RoomAvailable.new
-				@room.days = @days
+				@room = RoomAvailable.find_by_room_type_id(room[0])
+				if @room.nil?
+					@room = RoomAvailable.new
+				end
 				@room.room_type_id = room[0]
-				@room.number = params[:rooms_to_sell]
+				if params[:closed] == "0"
+					@room.number = 0
+					@weekdays = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+					if !params[:days].blank?
+						params[:days].each do |day|
+							@weekdays.delete_at(@weekdays.index(day[0]))
+						end
+					end
+					@weekdays.each do |day|
+						@days << day
+					end
+				elsif !params[:closed] || params[:closed] == "1"
+					@room.number = 0
+					if !params[:days].blank? 
+						params[:days].each do |day|
+							@days << day[0]
+						end
+					else
+						@days = ""
+					end
+				end
+				@room.days = @days
 				if !params[:to_date].blank? 
 					@room.to_date = DateTime.strptime(params[:to_date], '%m/%d/%Y').to_date
 				else
@@ -26,13 +49,6 @@ layout "admin_basic"
 					@room.from_date = DateTime.strptime(params[:from_date], '%m/%d/%Y').to_date
 				else
 					@room.from_date = ""
-				end
-				if !params[:days].blank? 
-					params[:days].each do |day|
-						@days << day[0]
-					end
-				else
-					@days = ""
 				end
       	@room.save
 			end
@@ -46,7 +62,7 @@ layout "admin_basic"
 	end
 
 	def edit
-		@room = RoomAvailable.find(1)
+		@room = RoomAvailable.find(5)
 	end
 
 	def update_status
