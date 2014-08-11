ad74776457e1f14d9c5232cc5ae6b0d70aa3876f
surajit-int <surajit.dey@indusnet.co.in>
2014-08-11 14:04:39 +0530

    Rooms section breakage fixes added

--- app/models/room.rb
+++ app/models/room.rb
@@ -15,7 +15,7 @@ class Room < ActiveRecord::Base
   has_many :room_attribute_joins, dependent: :destroy
   has_many :room_attributes, through: :room_attribute_joins
   has_many :room_rate_details, dependent: :destroy
-  has_many :room_rates, dependent: :destroy
+  has_many :room_rates
   has_many :room_availables, dependent: :destroy
   
   def self.latest_rooms
