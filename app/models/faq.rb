d643bf7d48f67249577eb4b638937489c7d5ada4
surajit-int <surajit.dey@indusnet.co.in>
2014-10-27 18:18:48 +0530

    faq category validations added

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,4 +1,6 @@
 class Faq < ActiveRecord::Base
   attr_accessible :answer, :question, :category_id
   belongs_to :faq_category
+  validates :question, presence: true
+  validates :answer, presence: true
 end
--- app/models/faq_category.rb
+++ app/models/faq_category.rb
@@ -1,4 +1,5 @@
 class FaqCategory < ActiveRecord::Base
   attr_accessible :name
-  has_many :faq
+  has_many :faqs, dependent: :destroy
+  validates :name, presence: true
 end
